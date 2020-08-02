85b558653ec1c224679c0f127ef4fb7bb7126356
manami-project <13179375+manami-project@users.noreply.github.com>
2020-08-02 10:17:31 +0200

    Updated user-agents

--- src/main/kotlin/io/github/manamiproject/modb/core/httpclient/HeaderCreator.kt
+++ src/main/kotlin/io/github/manamiproject/modb/core/httpclient/HeaderCreator.kt
@@ -14,7 +14,10 @@ internal object HeaderCreator {
         val headers = mutableMapOf<String, String>(
                 Pair("Host", url.host),
                 Pair("Connection", "keep-alive"),
-                Pair("Upgrade-Insecure-Requests", "1")
+                Pair("Upgrade-Insecure-Requests", "1"),
+                Pair("Pragma", "no-cache"),
+                Pair("Cache-Control", "no-cache"),
+                Pair("TE", "Trailers")
         )
         headers.putAll(addBrowserUnspecificHeaders())
 
@@ -29,25 +32,23 @@ internal object HeaderCreator {
     }
 
     private fun addBrowserUnspecificHeaders(): Map<String, String> {
-        val languages = listOf("de,en-US;q=0.7,en;q=0.3", "en-US,en;q=0.5", "en-US,en;q=0.8") // remove german
+        val languages = listOf("en-US,en;q=0.8")
 
         return mapOf(Pair("Accept-Language", languages.pickRandom()))
     }
 
     private fun addFirefoxSpecificHeaders(): Map<String, String> {
         val headers = mutableMapOf(
-                Pair("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"),
+                Pair("Accept", "*/*"),
                 Pair("DNT", "1")
         )
 
         val userAgents = listOf(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Linux i686; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
         )
 
         headers["User-Agent"] = userAgents.pickRandom()
--- src/test/kotlin/io/github/manamiproject/modb/core/httpclient/HeaderCreatorTest.kt
+++ src/test/kotlin/io/github/manamiproject/modb/core/httpclient/HeaderCreatorTest.kt
@@ -16,17 +16,18 @@ internal class HeaderCreatorTest {
         assertThat(result["host"]).isEqualTo("localhost")
         assertThat(result["connection"]).isEqualTo("keep-alive")
         assertThat(result["upgrade-insecure-requests"]).isEqualTo("1")
-        assertThat(result["accept-language"] in listOf("de,en-US;q=0.7,en;q=0.3", "en-US,en;q=0.5", "en-US,en;q=0.8")).isTrue()
-        assertThat(result["accept"]).isEqualTo("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+        assertThat(result["accept-language"] in listOf("en-US,en;q=0.8")).isTrue()
+        assertThat(result["accept"]).isEqualTo("*/*")
         assertThat(result["dnt"]).isEqualTo("1")
+        assertThat(result["pragma"]).isEqualTo("no-cache")
+        assertThat(result["cache-control"]).isEqualTo("no-cache")
+        assertThat(result["te"]).isEqualTo("Trailers")
         assertThat(result["user-agent"] in listOf(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Linux i686; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
         )).isTrue()
     }
 
@@ -39,7 +40,10 @@ internal class HeaderCreatorTest {
         assertThat(result["host"]).isEqualTo("localhost")
         assertThat(result["connection"]).isEqualTo("keep-alive")
         assertThat(result["upgrade-insecure-requests"]).isEqualTo("1")
-        assertThat(result["accept-language"] in listOf("de,en-US;q=0.7,en;q=0.3", "en-US,en;q=0.5", "en-US,en;q=0.8")).isTrue()
+        assertThat(result["accept-language"] in listOf("en-US,en;q=0.8")).isTrue()
+        assertThat(result["pragma"]).isEqualTo("no-cache")
+        assertThat(result["cache-control"]).isEqualTo("no-cache")
+        assertThat(result["te"]).isEqualTo("Trailers")
         assertThat(result["user-agent"] in listOf(
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
             "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
