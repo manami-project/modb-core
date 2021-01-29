607bfed29691a9e547f7984adeaa6fbedae2b8ac
manami-project <13179375+manami-project@users.noreply.github.com>
2021-01-29 19:49:38 +0100

    Updated user agents

--- src/test/kotlin/io/github/manamiproject/modb/core/httpclient/HeaderCreatorTest.kt
+++ src/test/kotlin/io/github/manamiproject/modb/core/httpclient/HeaderCreatorTest.kt
@@ -9,49 +9,59 @@ internal class HeaderCreatorTest {
 
     @Test
     fun `check header for firefox`() {
+        // given
+        val url = URL("http://localhost:8080")
+
         //when
-        val result = HeaderCreator.createHeadersFor(URL("http://localhost:8080"), Firefox)
+        val result = HeaderCreator.createHeadersFor(url, Firefox)
 
         // then
         assertThat(result["host"]).isEqualTo("localhost")
         assertThat(result["connection"]).isEqualTo("keep-alive")
         assertThat(result["upgrade-insecure-requests"]).isEqualTo("1")
-        assertThat(result["accept-language"] in listOf("en-US,en;q=0.8")).isTrue()
+        assertThat(result["accept-language"]).isIn(listOf("en-US,en;q=0.8"))
         assertThat(result["accept"]).isEqualTo("*/*")
         assertThat(result["dnt"]).isEqualTo("1")
         assertThat(result["pragma"]).isEqualTo("no-cache")
         assertThat(result["cache-control"]).isEqualTo("no-cache")
         assertThat(result["te"]).isEqualTo("Trailers")
-        assertThat(result["user-agent"] in listOf(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.1; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Mozilla/5.0 (X11; Linux i686; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Mozilla/5.0 (Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0",
-        )).isTrue()
+        assertThat(result["user-agent"]).isIn(
+            listOf(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.1; rv:85.0) Gecko/20100101 Firefox/85.0",
+                "Mozilla/5.0 (X11; Linux i686; rv:85.0) Gecko/20100101 Firefox/85.0",
+                "Mozilla/5.0 (Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+                "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:85.0) Gecko/20100101 Firefox/85.0",
+                "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+                "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+            )
+        )
     }
 
     @Test
     fun `check header for chromium`() {
+        // given
+        val url = URL("http://localhost:8080")
+
         //when
-        val result = HeaderCreator.createHeadersFor(URL("http://localhost:8080"), Chromium)
+        val result = HeaderCreator.createHeadersFor(url, Chromium)
 
         // then
         assertThat(result["host"]).isEqualTo("localhost")
         assertThat(result["connection"]).isEqualTo("keep-alive")
         assertThat(result["upgrade-insecure-requests"]).isEqualTo("1")
-        assertThat(result["accept-language"] in listOf("en-US,en;q=0.8")).isTrue()
+        assertThat(result["accept-language"] ).isIn(listOf("en-US,en;q=0.8"))
         assertThat(result["pragma"]).isEqualTo("no-cache")
         assertThat(result["cache-control"]).isEqualTo("no-cache")
         assertThat(result["te"]).isEqualTo("Trailers")
-        assertThat(result["user-agent"] in listOf(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-        )).isTrue()
+        assertThat(result["user-agent"]).isIn(
+            listOf(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+            )
+        )
     }
 }
