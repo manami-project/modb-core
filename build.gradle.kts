449ac2938af06dde3eb119eff28190543d4aa435
manami-project <13179375+manami-project@users.noreply.github.com>
2021-06-11 17:31:53 +0200

    Updated gson to 2.8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 
     implementation(platform(kotlin("bom")))
     implementation("com.beust:klaxon:5.5")
-    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.code.gson:gson:2.8.7")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
