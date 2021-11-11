714e4b2552c8ba64cbc303320a592a3b30dd7505
manami-project <13179375+manami-project@users.noreply.github.com>
2021-11-11 19:47:54 +0100

    Updated gradle to 7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,16 +40,11 @@ kotlin {
     explicitApi()
 }
 
-val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = Versions.JVM_TARGET
-    freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
-}
-
-val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = Versions.JVM_TARGET
-    freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = Versions.JVM_TARGET
+        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
 }
 
 tasks.withType<Test> {
