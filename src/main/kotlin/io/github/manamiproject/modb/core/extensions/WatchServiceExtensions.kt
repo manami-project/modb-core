6b313f0e175fdfbd1ab62a639a366a665bbedf33
manami-project <13179375+manami-project@users.noreply.github.com>
2022-11-04 22:05:40 +0100

    Reduced stress on system when using longPoll()

--- src/main/kotlin/io/github/manamiproject/modb/core/extensions/WatchServiceExtensions.kt
+++ src/main/kotlin/io/github/manamiproject/modb/core/extensions/WatchServiceExtensions.kt
@@ -1,23 +1,26 @@
 package io.github.manamiproject.modb.core.extensions
 
 import io.github.manamiproject.modb.core.coroutines.ModbDispatchers.LIMITED_FS
+import io.github.manamiproject.modb.core.random
 import kotlinx.coroutines.*
-import java.nio.file.ClosedWatchServiceException
-import java.nio.file.WatchKey
-import java.nio.file.WatchService
+import java.nio.file.*
+import kotlin.time.DurationUnit.SECONDS
+import kotlin.time.toDuration
 
 /**
  * Retrieve [WatchKey]s and prevent [ClosedWatchServiceException] to be thrown. Instead of throwing the exception `null` is being returned.
  * @since 8.0.0
  * @return Either a [WatchKey] or `null`
  */
-public suspend fun WatchService.longPoll(): WatchKey? { // FIXME: create custom key in order to prevent nullable return value?
+public suspend fun WatchService.longPoll(): WatchKey? {
     val watchService = this
     return withContext(LIMITED_FS) {
         try {
             var key: WatchKey? = null
+            var delay = random(3, 6).toDuration(SECONDS)
             while (key == null && isActive) {
-                yield()
+                if (delay < 30.toDuration(SECONDS)) delay = delay.plus(1.toDuration(SECONDS))
+                delay(delay)
                 key = watchService.poll()
             }
             key
