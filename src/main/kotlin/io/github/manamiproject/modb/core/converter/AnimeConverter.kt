351536c65e783160f68300a7cfbd08003e451b1c
manami-project <13179375+manami-project@users.noreply.github.com>
2024-04-12 16:35:22 +0200

    Added KDoc

--- src/main/kotlin/io/github/manamiproject/modb/core/converter/AnimeConverter.kt
+++ src/main/kotlin/io/github/manamiproject/modb/core/converter/AnimeConverter.kt
@@ -3,6 +3,17 @@ package io.github.manamiproject.modb.core.converter
 import io.github.manamiproject.modb.core.models.Anime
 
 /**
+ * @since 11.1.0
+ */
+public typealias OutputKey = String
+
+/**
+ * @since 11.1.0
+ */
+public typealias Selector = String
+
+
+/**
  * Converts raw content in form of a [String] into an [Anime].
  * @since 1.0.0
  */
@@ -17,8 +28,14 @@ public interface AnimeConverter {
      */
     public suspend fun convert(rawContent: String): Anime
 
+    /**
+     * Extracts certain information either using XPath for HTML/XML or JsonPath for JSON.
+     * @since 11.1.0
+     * @param rawContent The raw content which will be converted to an [Anime]. This can be either HTML/XML or JSON.
+     * @param selection A [Map] defining selectors. The key is the name of the identifier in the result set. The value
+     * is either the XPath or the JsonPath string which identifies the data to select from the [rawContent].
+     * @return A [Map] containing the resulting data. Key is the identifier corresponding to the key from [selection].
+     * The value is the identified data from [rawContent] based on the XPath or JsonPath.
+     */
     public suspend fun convert(rawContent: String, selection: Map<OutputKey, Selector>): Map<OutputKey, Any>
 }
-
-public typealias OutputKey = String
-public typealias Selector = String
--- src/main/kotlin/io/github/manamiproject/modb/core/converter/PathConverter.kt
+++ src/main/kotlin/io/github/manamiproject/modb/core/converter/PathConverter.kt
@@ -18,5 +18,13 @@ public interface PathConverter {
      */
     public suspend fun convert(path: Path): Collection<Anime>
 
+    /**
+     * Extracts data from a file or multiple files of a directory either using XPath for HTML/XML or JsonPath for JSON.
+     * @since 11.1.0
+     * @param path Can either be a file or a directory.
+     * @return A [Collection] of [Map]s containing the resulting data. Key is the identifier corresponding to the key
+     * from [selection]. The value is the identified data from files based on the XPath or JsonPath.
+     * @throws IllegalArgumentException if the given [Path] is neither file nor directory.
+     */
     public suspend fun convert(path: Path, selection: Map<OutputKey, Selector>): Collection<Map<OutputKey, Any>>
 }
