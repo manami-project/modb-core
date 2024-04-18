ff6f63734729bb1e007873994e3e945911e81489
manami-project <13179375+manami-project@users.noreply.github.com>
2024-04-18 20:52:37 +0200

    Added KDoc

--- src/main/kotlin/io/github/manamiproject/modb/core/extractor/JsonDataExtractor.kt
+++ src/main/kotlin/io/github/manamiproject/modb/core/extractor/JsonDataExtractor.kt
@@ -3,7 +3,10 @@ package io.github.manamiproject.modb.core.extractor
 import com.nfeld.jsonpathkt.JsonPath
 import com.nfeld.jsonpathkt.extension.read
 
-
+/**
+ * Extract data using JsonPath.
+ * @since 12.0.0
+ */
 public object JsonDataExtractor : DataExtractor {
 
     override suspend fun extract(rawContent: String, selection: Map<OutputKey, Selector>): ExtractionResult {
